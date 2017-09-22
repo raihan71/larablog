886858550a3d1fc9639a973742242c30025d6114
raihannismara <raihannismara@gmail.com>
2017-09-22 16:43:09 +0700

    Fix bugs

---
+++ database/migrations/2017_09_22_074301_create_table_posts.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablePosts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table){
+            $table->increments('id');
+            $table->integer('cat_id');
+            $table->string('title');
+            $table->string('desc');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
