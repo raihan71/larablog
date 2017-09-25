6d2077ef9cac52e8013ed36c72d8bd9968868646
raihannismara <raihannismara@gmail.com>
2017-09-25 08:03:51 +0700

    Fix blade

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -38,7 +38,7 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        $post = new post;
+        $post = new Post;
         $post->title = $request->get('title');
         $post->desc = $request->get('content');
        if($request->hasFile('image')){
@@ -85,7 +85,7 @@ class PostController extends Controller
      */
     public function update(Request $request, $id)
     {
-       $post = post::find($id);
+       $post = Post::find($id);
        $post->title = $request->get('title');
        $post->desc = $request->get('content');
        if($request->hasFile('image')){
@@ -110,7 +110,7 @@ class PostController extends Controller
     public function destroyall(Request $request)
     {
         if(count(collect($request->checkbox)) > 1){
-          $post = post::whereIn('id',$request->get('checkbox'));
+          $post = Post::whereIn('id',$request->get('checkbox'));
           $post->delete();
         }else{
           $post = post::find($request->get('checkbox'))->first();
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,33 +1,33 @@
 @extends('layouts.visitor-layout')
 @section('content')
 <!-- Blog Entries Column -->
-        <div class="col-md-12 col-sm-12 col-xs-12" style="text-align: left;">
-        <h1>News
-        </h1>
+<div class="col-md-12 col-sm-12 col-xs-12" style="text-align: left;">
+  <h1>News
+  </h1>
 
-        <!-- Blog Post -->
-        @foreach($post as $data)
-        <div class="card mb-4">
-            <div class="card-body">
-              <h2 class="card-title"><a href="{{ url('post/'.$data->id)}}">{{$data->title}}</a></h2>
-              <img src="{{ URL::asset('image/post/'.$data->image)}}" alt="{{$data->title}}">
-              <p class="card-text">{{url($data->desc)}}</p>
-              <a href="{{ url('/'.$data->id)}}" class="btn btn-primary">Read More &rarr;</a>
-          </div>
-          <div class="card-footer text-muted">
-           {{$data->created_at}}
-           <a href="#">Admin</a>
-       </div>
+  <!-- Blog Post -->
+  @foreach($post as $data)
+  <div class="card mb-4">
+    <div class="card-body">
+      <h2 class="card-title"><a href="{{ url('post/'.$data->id)}}">{{$data->title}}</a></h2>
+      <img src="{{ URL::asset('image/post/'.$data->image)}}" alt="{{$data->title}}">
+      <p class="card-text">{{str_limit($data->desc, $limit = 500, $end = '...')}}</p>
+      <a href="{{ url('/'.$data->id)}}" class="btn btn-primary">Read More &rarr;</a>
+    </div>
+    <div class="card-footer text-muted">
+     {{$data->created_at}}
+     <a href="#">Admin</a>
    </div>
-   @endforeach
+ </div>
+ @endforeach
 
-   <!-- Pagination -->
-   <ul class="pagination justify-content-center mb-4">
-    <li class="page-item">
-      <a class="page-link" href="#">&larr; Older</a>
+ <!-- Pagination -->
+ <ul class="pagination justify-content-center mb-4">
+  <li class="page-item">
+    <a class="page-link" href="#">&larr; Older</a>
   </li>
   <li class="page-item disabled">
-      <a class="page-link" href="#">Newer &rarr;</a>
+    <a class="page-link" href="#">Newer &rarr;</a>
   </li>
 </ul>
 
